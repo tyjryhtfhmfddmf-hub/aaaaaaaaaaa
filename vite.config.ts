756e56d935c9d3370a6e99703e82588031e247fc
tyjryhtfhmfddmf-hub <tyjryhtfhmfddmf@gmail.com>
2025-11-13 11:54:12 +0000

    new main

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: './',
   plugins: [react()],
+  esbuild: {
+    jsxInject: `import React from 'react'`
+  },
   server: {
     port: 5173,
     strictPort: true,
